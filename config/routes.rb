c67df06c10cf943bea10a6359f2a185fd345d9c0
Nickkang257 <nickkang257@gmail.com>
2015-07-06 19:41:21 -0500

    added basic education controller with routes, action, views

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "users#index"
 
+  get      "/education"          => 'educations#index'
+  get      "/education/new"      => 'educations#new'
+  post     "/education"          => 'educations#create'
+  get      "/education/:id/edit" => 'educations#edit'
+  get      "/education/:id"      => 'educations#show'
+  patch    "/education/:edit"    => 'educations#update'
+  delete   "/education/:id"      => 'educations#destroy'
+
 end
