464fddc83353858b7511b51aec34ed1d96d4642c
Charles Warsh <charles.warsh@brickfish.com>
2015-07-06 19:53:56 -0500

    I just migrated, I have no idea what needed to be migrated, apparently User model from Devise

--- app/controllers/attributes_controller.rb
+++ app/controllers/attributes_controller.rb
@@ -4,4 +4,27 @@ class AttributesController < ApplicationController
     @student_attributes = Attribute.all
 
   end
+
+  def show
+    @student_attribute = Attribute.find_by(params[:id])
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+  def update
+
+  end
+
+
 end
