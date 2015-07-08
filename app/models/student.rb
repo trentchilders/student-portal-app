dda5849cae11f991b672a3c77eedae818350e3e6
Charles Warsh <charles.warsh@brickfish.com>
2015-07-07 20:37:58 -0500

    more stuff on the show page

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,6 @@
 class Student
 
-attr_accessor :first_name, :last_name, :id, :email, :phone_number, :short_bio, :linkedin, :twitter, :blog_site, :resume, :github, :photo, :start_date, :end_date, :degree, :university_name, :details, :job_title, :company_name
+attr_accessor :first_name, :last_name, :id, :email, :phone_number, :short_bio, :linkedin, :twitter, :blog_site, :resume, :github, :photo, :start_date, :end_date, :degree, :university_name, :details, :job_title, :company_name, :name, :description, :url, :screenshot
 
   def initialize(hash)
       @first_name = hash["first_name"]
@@ -22,6 +22,10 @@ attr_accessor :first_name, :last_name, :id, :email, :phone_number, :short_bio, :
       @details = hash["details"]
       @job_title = hash["job_title"]
       @company_name = hash["company_name"]
+      @name = hash["name"]
+      @description = hash["description"]
+      @url = hash["url"]
+      @screenshot = hash["screenshot"]
   end
 
   def self.find(id)
